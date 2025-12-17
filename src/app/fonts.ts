6d042b148e581d1616392d30d19ccf1d6c713e2c
NoahRGB <ncox2004@gmail.com>
2025-12-17 11:00:58 +0000

    strip back

--- src/app/fonts.ts
+++
@@ -1,6 +0,0 @@
-import { Inter, Lusitana, Edu_VIC_WA_NT_Beginner, Ubuntu } from "next/font/google";
-
-export const inter = Inter({subsets: ["latin"]});
-export const lusitana = Lusitana({weight: ["400", "700"], subsets: ["latin"]});
-export const edu = Edu_VIC_WA_NT_Beginner({weight:["400", "600", "700"], subsets: ["latin"]});
-export const ubuntu = Ubuntu({weight:["400", "500", "700"], subsets: ["latin"]});
